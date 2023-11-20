b12364d92746df03ad7ef16f204478829e7a6707
Fernandafmsf <fernandafernandesauto@gmail.com>
2023-11-20 20:49:33 -0300

    creating new views

--- resources/views/jogos/index.blade.php
+++ resources/views/jogos/index.blade.php
@@ -5,7 +5,33 @@
 
 @section('content')
  <!--Tudo aqui dentro será renderizado lá no template (layouts.app)-->
-<h1>Teste</h1>
+
+ <h2 class="container-md" >Bem vindo!</h2>
+ <p class=" container-md">Aqui é possível armazenar seus jogos favoritos! Adicione o nome, ano que jogou pela primeira vez e comentários!Não se esqueça de mencionar o que mais gostou!
+  </p>
+
+
+  <form method= "POST" class="container-md">
+    <div class="mb-3">
+      <label for="nome">Nome do jogo: </label>
+      <input type="text" name="nome" class="form-control">
+    </div>
+
+    <div class="mb-3">
+      <label for="ano">Ano que jogou: </label>
+      <input type="date" name="ano" class="form-control">
+    </div>
+
+    <div class="mb-3">
+      <label for="comentario">Comentários: </label>
+      <textarea name="comentario" cols="30" rows="5" class="form-control" placeholder="Comente sobre o jogo..."></textarea>
+    </div>
+  </form>
 
 
 @endsection
+
+
+
+
+  
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,30 @@
   <title>@yield('title') - GN </title>
 </head>
 <body>
+  
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid m-2">
+      <a class="navbar-brand" href="/jogos">Cadastro de Jogos</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class=" navbar-collapse collapse" id="navbarNav">
+  
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="/home">Listar</a>
+          </li>
+          
+        </ul>
+  
+      </div>
+    </div>
+  </nav>
  
   @yield('content')
   
 </body>
 </html>
+
